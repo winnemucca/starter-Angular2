75614cad78ae88493d10dd9f9983e980e7c1357d
winnemucca <skauyedauty@gmail.com>
2016-04-25 16:48:29 -0600

    router setup issues nearly fixed

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from 'angular2/core';
+import { Component } from 'angular2/core';
 import 'rxjs/Rx';   // Load all features
 import { ROUTER_PROVIDERS, RouteConfig, ROUTER_DIRECTIVES } from 'angular2/router';
 
@@ -8,13 +8,17 @@ import { WelcomeComponent } from '../app/home/welcome.component';
 
 @Component({
     selector: 'my-app',
-    templateUrl: './app/app.component.html'
+    templateUrl: './app/app.component.html',
+    directives: [ROUTER_DIRECTIVES]
+    //providers: [ROUTER_PROVIDERS]
 })
 
 @RouteConfig([
-    {path: '/welcome', name: 'Welcome', component: WelcomeComponent, useAsDefault: true}        
+    {path: '/welcome', name: 'Welcome', component: WelcomeComponent }        
 ])
 export class AppComponent { 
-        pageTitle: string = 'Calorie Intake';
+    pageTitle: string = 'Calorie Intake';
+
+    constructor () {}
 
 }
--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,5 @@
 import {bootstrap}    from 'angular2/platform/browser';
+// main component
 import {AppComponent} from './app.component';
 
 bootstrap(AppComponent);
