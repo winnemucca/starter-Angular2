92372b2c21200737ca87b8bfdb806a60d6e81201
winnemucca <skauyedauty@gmail.com>
2016-05-01 07:38:06 -0600

    created star component

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -1,8 +1,17 @@
 import {Component } from 'angular2/core';
+import { IProduct } from './product';
+import { StarComponent } from '../shared/star.component';
+
+import { ROUTER_DIRECTIVES } from 'angular2/router';
+
+
+
 
 @Component({
     templateUrl: 'app/products/product-list.component.html',
-    styleUrls: ['app/products/product-list.component.css']
+    styleUrls: ['app/products/product-list.component.css'],
+    directives: [StarComponent, ROUTER_DIRECTIVES]
+
 })
 
 export /**
